e34b26e21ef34bb46eb1c152a77e7c232e4b0f5b
Roelof Noort <roelofnoort@yandex.com>
2020-06-23 00:03:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,98 @@
-const express = require('express')
-const ffmpeg = require('fluent-ffmpeg')
-const fs = require('fs')
+const express = require('express'); // Express
+const ffmpeg = require('fluent-ffmpeg'); // Fluent-ffmpeg
+const fs = require('fs'); // Fs
 
-const app = express()
-const htmlerror = __dirname + "/webpage/error.html"
-const port = 80
+// App
+const app = express();
 
+// Configuration
+const config = {
+    port: 80,
+    ffmpegLocation: "C:\\ffmpeg\\bin\\ffmpeg.exe"
+};
+
+// Check if ffmpeg exists there
+fs.access(config.ffmpegLocation, fs.F_OK, (err) => { if(err) return console.log("FFMPEG not installed properly"); });
+
+// Pages
+const commaPage = __dirname + "/webpage/komma.html",
+      errorPage = __dirname + "/webpage/error.html";
+
+// Router
 app.get('/crabbo', function(req, res)  {
-    var crabtext = req.query.text
     if (res.statusCode = 200) {
-        if (!crabtext) {
-            res.sendFile(htmlerror)
-            return
-        } else {
-            generate(crabtext, res)
-        }  
-    } else {
-        res.send("there was an error")
-    }
-})
+        if (!req.query.text) {
+            return res.sendFile(errorPage);
+        } else generate(req.query.text, res);
+    } else res.send("There was an error");
+});
 
-app.listen(port, () => {
-    console.log(`Server started at port ${port}`)
-    return
-})
 
-function generate(crabtext, res) {
+// Generate function
+function generate(text, res) {
+    var crabboFile  = __dirname + "/data/" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + ".mp4"; // Random file name
+    text = text.split("[").join("(").split("]").join(")");
+    if(!text.includes(",")) return res.sendFile(commaPage); // If the text doesn't include a comma, send the commapage back
+
+    var text = text.trim(),
+        text = text.split(",")
+        text1L = text[0].trim(),
+        text1L = text1L.length,
+        text2L = text[1].trim(),
+        text2L = text2L.length;
+
     try {
-    var inputcrab = __dirname + "/data/crab.mp4"
-    var kommapage = __dirname + "/webpage/komma.html"
-    var crabbomp4 = __dirname + "/data/" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + ".mp4"
-    var text = crabtext.split("[").join("(").split("]").join(")")
-    var comma = text.includes(",")
-    if (!comma == true) {
-        res.sendFile(kommapage)
-        return
-    } else {
-    var text = text.trim()
-    var text = text.split(",")
-    var proc = ffmpeg()
-    var text1L = text[0].trim()
-    var text1L = text1L.length
-    var text2L = text[1].trim()
-    var text2L = text2L.length
+        var proc = ffmpeg();
+        var input = __dirname + "/data/crab.mp4";
+        proc.setFfmpegPath(config.ffmpegLocation)
+            .input(input)
+            .videoBitrate('700')
+            .addOption('-preset', 'veryfast')
+            .inputOption('-threads', '16')
 
-proc.setFfmpegPath("C:\\ffmpeg\\bin\\ffmpeg.exe")
-    .input(inputcrab)
-    .videoBitrate('700')
-    .addOption('-preset', 'veryfast')
-    .inputOption('-threads', '16')
-
-    .on('end', function() {
-        res.setHeader('Content-Type', 'video/mp4')
-        res.download(crabbomp4, "crab.mp4", function(err) {
-            if (err) { console.log(err)
-            } else {
-                console.log("successfully sended crabrave with title: " + '"' + crabtext + '"')
-                res.end()
-                fs.unlinkSync(crabbomp4)
-                return
-                }
+            .on('end', function() {
+                res.setHeader('Content-Type', 'video/mp4');
+                res.download(crabboFile, "crab.mp4", function(err) {
+                    if (err) return console.error(err);
+                    console.log("Successfully sended crabrave with title: " + '"' + text + '"');
+                    res.end();
+                    fs.unlinkSync(crabboFile);
+                })
             })
-        })
+            .on('error', function(err) {
+                console.log('An error occured: ' + err.message);
+                fs.unlinkSync(crabboFile);
+            })
+            .videoFilters({
+                filter: 'drawtext',
+                options: {
+                    fontfile: './data/hacked.ttf',
+                    text: `${text[0]}`,
+                    fontsize: '60',
+                    x: (856/2-30*text1L/2),
+                    y: 'H-240',
+                    fontcolor: 'white',
+                    shadowcolor: 'black',
+                    shadowx: '2',
+                    shadowy: '2'
+                }
+            }, {
+                filter: 'drawtext',
+                    options: {
+                        fontfile: './data/hacked.ttf',
+                        text: `${text[1]}`,
+                        fontsize: '60',
+                        x: (856/2-30*text2L/2),
+                        y: 'H-170',
+                        fontcolor: 'white',
+                        shadowcolor: 'black',
+                        shadowx: '2',
+                        shadowy: '2'
+                    }
+                }
+            ).save(crabboFile);
+    } catch(e) { console.error(e); };
+};
 
-    .on('error', function(err) {
-        console.log('an error happened: ' + err.message)
-        fs.unlinkSync(crabbomp4)
-    })
-
-  .videoFilters({
-    filter: 'drawtext',
-    options: {
-      fontfile: './data/hacked.ttf',
-      text: `${text[0]}`,
-      fontsize: '60',
-      x: (856/2-30*text1L/2),
-      y: 'H-240',
-      fontcolor: 'white',
-      shadowcolor: 'black',
-      shadowx: '2',
-      shadowy: '2'
-    }},{
-  filter: 'drawtext',
-    options: {
-      fontfile: './data/hacked.ttf',
-      text: `${text[1]}`,
-      fontsize: '60',
-      x: (856/2-30*text2L/2),
-      y: 'H-170',
-      fontcolor: 'white',
-      shadowcolor: 'black',
-      shadowx: '2',
-      shadowy: '2'
-    }})
-    .save(crabbomp4)
-    }} catch(err) {console.log(err)}
-}
+// Listen
+app.listen(config.port, () => { console.log(`Server started at port: ${config.port}`); });
